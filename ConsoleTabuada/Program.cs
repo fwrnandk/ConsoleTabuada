ee23bee45ec18fcd79b054ccd6ad12c0ef5782a4
Fernando Santos <122576682+fwrnandk@users.noreply.github.com>
2024-08-14 11:48:29 -0300

    Add files via upload

---
+++ ConsoleTabuada/Program.cs
@@ -0,0 +1,14 @@
+﻿using ModuloCalculadora;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+Console.WriteLine("Digite o número para calcular a tabuada:");
+int numero = int.Parse(Console.ReadLine());
+
+Tabuada tabuada = new Tabuada(numero);
+string resultado = tabuada.Calcular(numero);
+
+Console.WriteLine(resultado);
